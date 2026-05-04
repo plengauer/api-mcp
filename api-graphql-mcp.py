fa64532b41fe2a49b1a2fafc50255a58da0c2958
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-05-04 13:17:46 +0200

    Refactor GraphQLMCP initialization syntax

--- api-graphql-mcp.py
+++ api-graphql-mcp.py
@@ -1,13 +1,12 @@
 import os
 from graphql_mcp.server import GraphQLMCP
 
-
 mcp = GraphQLMCP.from_remote_url(
-    url=os.environ["API_MCP_BASE_URL"],
-    headers={
+    url = os.environ["API_MCP_BASE_URL"],
+    headers = {
         "Authorization": os.environ["API_MCP_AUTHORIZATION"]
     },
-    name=os.environ["API_MCP_SERVER_NAME"]
+    name = os.environ["API_MCP_SERVER_NAME"]
 )
 
 if __name__ == "__main__":
